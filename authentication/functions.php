1aeca088619f1c27086c8d6b71a674db22df1524
QuentinSpilmont <q.spilmont@iia-laval.fr>
2023-02-24 16:56:13 +0100

    Correction Broken Access

--- authentication/functions.php
+++ authentication/functions.php
@@ -3,7 +3,7 @@
 function connectDb()
 {
     try {
-        $conn = new PDO("mysql:host=127.0.0.1;dbname=authentication", 'root', '');
+        $conn = new PDO("mysql:host=127.0.0.1:8889;dbname=authentication", 'test', 'test');
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
@@ -13,14 +13,30 @@ function connectDb()
     }
 }
 
-function logUser($email, $password)
-{
+function getUserPassword($email) {
     $connexion = connectDb();
-    $sql = 'SELECT * FROM users WHERE email = "' . $email . '" AND password = "' .$password . '"';
+    $sql = 'SELECT * FROM users WHERE email = "' . $email . '"';
     $stmt = $connexion->prepare($sql);
     $stmt->execute();
 
     return $stmt->fetchAll(PDO::FETCH_OBJ);
+}
+
+
+function logUser($email, $password)
+{
+    $connexion = connectDb();
+    $sql = $connexion->prepare('SELECT * FROM users WHERE email = ?');
+    $sql->bindParam(1, $email);
+    $sql->execute();
+
+    $user = $sql->fetch(PDO::FETCH_ASSOC);
+    if(password_verify($password,$user['password'])) {
+        return $user;
+    } else {
+        return null;
+    };
+    
 }
 
 function getUser($id) {
@@ -34,7 +50,7 @@ function getUser($id) {
 
 function saveUser($email, $username, $password) {
     $connexion = connectDb();
-    $sql = 'INSERT INTO users(username,email,password) VALUES("'.$email.'","'.$username.'","'.$password.'")';
+    $sql = 'INSERT INTO users(username,email,password) VALUES("'.htmlentities($email).'","'.htmlentities($username).'","'.password_hash($password, PASSWORD_DEFAULT).'")';
     $stmt = $connexion->prepare($sql);
 
     return $stmt->execute();
